25a3f7cd344e82f97222ce464d6c9945c19065e7
joão vitor Fedrizze <jh.2220@hotmail.com>
2023-10-17 02:12:57 -0300

    Dia 2

--- src/main/java/br/com/wolf/todolist/user/UserController.java
+++ src/main/java/br/com/wolf/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.wolf.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-        System.out.println(userModel.username);
-        System.out.println(userModel.password);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
